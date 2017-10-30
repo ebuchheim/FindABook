0c2e20dbb312c721ebd9891ba210b6056413f01a
Emma Buchheim <emma.buchheim@gmail.com>
2017-10-29 19:30:30 -0500

    Create BookAdapter with stubbed info

--- app/src/main/java/com/example/android/findabook/BookAdapter.java
+++ app/src/main/java/com/example/android/findabook/BookAdapter.java
@@ -1,8 +1,38 @@
 package com.example.android.findabook;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
 /**
  * Created by Emma Buchheim on 10/29/2017.
  */
 
-public class BookAdapter {
+public class BookAdapter extends ArrayAdapter<String> {
+
+    public BookAdapter(Activity context, ArrayList<String> books) {
+        super(context, 0, books);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+
+        //Check to see if there is a view we can reuse, and if not, create a new one
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        return listItemView;
+
+    }
 }
--- app/src/main/java/com/example/android/findabook/MainActivity.java
+++ app/src/main/java/com/example/android/findabook/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.android.findabook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BookAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Stub in some book data to test with
+        ArrayList<String> books = new ArrayList<String>();
+        books.add("Stubbed Book 1");
+        books.add("Stubbed Book 2");
+        books.add("Stubbed Book 3");
+
+        //Find our book list setup in the XML
+        ListView bookListView = (ListView) findViewById(R.id.list);
+
+        //Create a new adapter for our list of books
+        mAdapter = new BookAdapter(this, new ArrayList<String>(books));
+
+        bookListView.setAdapter(mAdapter);
     }
 }
