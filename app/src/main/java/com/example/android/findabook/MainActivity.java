f70681f18d96ebb57da39290cd138fa97cbf1b3b
Emma Buchheim <emma.buchheim@gmail.com>
2017-11-01 19:25:51 -0500

    Hook up search button to AsyncTask

--- app/src/main/java/com/example/android/findabook/MainActivity.java
+++ app/src/main/java/com/example/android/findabook/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.findabook;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,7 +21,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new BookAsyncTask().execute("urls");
+        Button search = (Button) findViewById(R.id.search_button);
+
+        search.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                new BookAsyncTask().execute("urls");
+            }
+        });
+
+
     }
 
     private class BookAsyncTask extends AsyncTask<String, Void, ArrayList<Book>> {
